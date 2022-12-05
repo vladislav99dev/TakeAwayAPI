ae3ffdf655c48995303d3c02a3dda7951e3fd3cc
vladislav99dev <vladislavcho99@abv.bg>
2022-12-05 09:12:20 +0200

    connect app with db function

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const db = require("./config/db");
+
 
 const v1RestaurantRouter = require("./v1/routes/restaurant");
 
@@ -8,6 +10,12 @@ const PORT = process.env.PORT || 3000;
 
 app.use("/api/v1/restaurants", v1RestaurantRouter);
 
-app.listen(PORT, () => {
-  console.log(`Api is up and running on ${PORT}`);
-});
+db(process.env.DB_CONNECTION_STRING)
+  .then((response) => {
+    app.listen(PORT, () => {
+      console.log(`Api is up and running on ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
