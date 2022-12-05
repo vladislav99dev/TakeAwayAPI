d0077a16cb8775cf81660eb43b74209a6c8c50a1
vladislav99dev <vladislavcho99@abv.bg>
2022-12-05 09:12:37 +0200

    add services for restaurant

---
+++ src/services/restaurant.js
@@ -0,0 +1,25 @@
+const getAllRestaurants = () => {
+    return;
+}
+const getOneRestaurant = () => {
+    return;
+}
+const createNewRestaurant = () => {
+    return;
+}
+const updateOneRestaurant = () => {
+    return;
+}
+const deleteOneRestaurant = () => {
+    return;
+}
+
+const services = {
+    getAllRestaurants,
+    getOneRestaurant,
+    createNewRestaurant,
+    updateOneRestaurant,
+    deleteOneRestaurant
+}
+
+module.exports = services;
