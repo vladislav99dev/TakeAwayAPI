e60e3c698b881c16abb1f60dd3d68db8a9527e07
vladislav99dev <vladislavcho99@abv.bg>
2022-12-15 17:07:16 +0200

    update Restaurant Schema

--- src/models/Restaurant.js
+++ src/models/Restaurant.js
@@ -1,6 +1,36 @@
 const { Schema, model } = require("mongoose");
 
-const restaurantSchema = new Schema({});
+const restaurantSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  location: {
+    city: {
+      type: String,
+      required: true,
+    },
+    street: {
+      type: String,
+      required: true,
+    },
+    unit: {
+      type: String,
+      required: true,
+    },
+  },
+  type: {
+    type: String,
+    enum: ["Pizza", "Sushi", "Grill", "Sweets", "Fish", "Burger"],
+    required: true,
+  },
+  foods: {
+    type: Array,
+  },
+  deliveryCost: {
+    type: String,
+  },
+});
 
 const Restaurant = model("Restaurant", restaurantSchema);
 
