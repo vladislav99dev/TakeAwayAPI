292909ffbedbb24bca23cf0bde3b433261b8e60a
vladislav99dev <vladislavcho99@abv.bg>
2022-12-16 09:41:31 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,23 +1,44 @@
+const data = {
+  name: "",
+  location: "somethinbg",
+  type: "23",
+  something: 22,
+  foods: "",
+  deliveryCost: ""
+};
 
-const location = {
-    name:"",
-    street:"somethinbg",
-    unit: "23",
-    something:22
-}
+
+const validateAllFields = (data) => {
+    data?.name && data?.location && data?.type && data?.foods && data?.deliveryCost ? true : false
+//   if (
+//     data.name &&
+//     data.location &&
+//     data.type &&
+//     data.foods &&
+//     data.deliveryCost
+//   )
+//     true;
+};
+
+
+
 
 const locationCheck = (location) => {
-    let isValid = false;
-    for (const value in location) {
-        if(Boolean(location[value]) && location[value].trim() !== "" && typeof location[value] === 'string') {
-            isValid = true;
-        } else {
-            isValid = false;
-            break
-        }
+  let isValid = false;
+  for (const value in location) {
+    if (
+      Boolean(location[value]) &&
+      location[value].trim() !== "" &&
+      typeof location[value] === "string"
+    ) {
+      isValid = true;
+    } else {
+      isValid = false;
+      break;
     }
-    return isValid;
-  };
+  }
+  return isValid;
+};
 
-  const returnValue = locationCheck(location);
-  console.log(returnValue);
+const returnValue = locationCheck(location);
+console.log(returnValue);
