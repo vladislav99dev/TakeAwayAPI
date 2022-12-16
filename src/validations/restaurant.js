7259656d4a746546a26289b930619ffe4de07421
vladislav99dev <vladislavcho99@abv.bg>
2022-12-16 09:41:29 +0200

    Update restaurant.js

--- src/validations/restaurant.js
+++ src/validations/restaurant.js
@@ -1,16 +1,30 @@
 const validateAllFields = (data) => {
-  if (
-    data.name &&
-    data.location &&
-    data.type &&
-    data.foods &&
-    data.deliveryCost
-  )
-    true;
+    data?.name && data?.location && data?.type && data?.foods && data?.deliveryCost ? true : false
+//   if (
+//     data.name &&
+//     data.location &&
+//     data.type &&
+//     data.foods &&
+//     data.deliveryCost
+//   )
+//     true;
 };
 const name = (name) => {
   return typeof name === "string";
 };
 const location = (location) => {
-    
-};
+    let isValid = false;
+    for (const value in location) {
+      if (
+        Boolean(location[value]) &&
+        location[value].trim() !== "" &&
+        typeof location[value] === "string"
+      ) {
+        isValid = true;
+      } else {
+        isValid = false;
+        break;
+      }
+    }
+    return isValid;
+  };
